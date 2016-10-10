a81ba604fd1e2c1d98f1271c315d7380c21f195d
KiranRambha <krocks4520@gmail.com>
2016-10-10 19:48:38 +0100

    Local Exchange Trading System - Initial Configuration
    Installed and configured the basic libraries like jQuery, Bootstrap.LESS, Angular. Also rearranged the libraries into respective folders for cleaner folder structure.

---
+++ LETS/LETS/Startup.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LETS.Startup))]
+namespace LETS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+
+        private void ConfigureAuth(IAppBuilder app)
+        {
+
+        }
+    }
+}
