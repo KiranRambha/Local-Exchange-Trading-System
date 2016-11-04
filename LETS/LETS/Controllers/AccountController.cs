81f995e0bd99163e9947ccd7924f719cc63b5a8a
Kiran <kiran.rambha.2013@live.rhul.ac.uk>
2016-11-04 15:40:29 +0000

    The Users can login to the system using their username and password. New users can register to the system. The system prevents duplication of the usernames and doesn't allow the registration with two same email addresses

--- LETS/LETS/Controllers/AccountController.cs
+++ LETS/LETS/Controllers/AccountController.cs
@@ -1,19 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using LETS.Models;
 using MongoDB.Driver;
-using LETS.Properties;
 using LETS.Helpers;
 using MongoDB.Bson;
-using MongoDB.Bson.IO;
-using System.Security.Claims;
 using Microsoft.AspNet.Identity;
 using Microsoft.Owin.Security;
-using System.Web.Security;
-using System.Web.Configuration;
+using System.Threading.Tasks;
 
 namespace LETS.Controllers
 {
@@ -29,7 +23,8 @@ namespace LETS.Controllers
             if (Request.IsAuthenticated)
             {
                 return RedirectToAction("Index", "Home");
-            } else
+            }
+            else
             {
                 return View();
             }
@@ -38,29 +33,48 @@ namespace LETS.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         [AllowAnonymous]
-        public ActionResult Login(LoginViewModel loginUser)
+        public async Task<ActionResult> Login(LoginViewModel loginUser)
         {
             if (loginUser != null && ModelState.IsValid)
             {
-                if (true)
+                var user = await Context.RegisteredUsers.Find(new BsonDocument {
+                    { "Account.UserName", loginUser.UserName }
+                }).ToListAsync();
+
+                PasswordHashAndSalt passowordEncription = new PasswordHashAndSalt();
+                loginUser.Password = passowordEncription.getHashedPassword(loginUser.Password);
+
+                if (user.Count > 0)
                 {
-                    UserAuthentication userAuthentication = new UserAuthentication();
+                    if (user[0].Account.UserName.Equals(loginUser.UserName) && user[0].Account.Password.Equals(loginUser.Password))
+                    {
+                        UserAuthentication userAuthentication = new UserAuthentication();
 
-                    string role = "admin";
+                        string role = "admin";
 
-                    var ident = userAuthentication.AuthenticateUser(loginUser.UserName, role);
+                        var ident = userAuthentication.AuthenticateUser(loginUser.UserName, role);
 
-                    HttpContext.GetOwinContext().Authentication.SignIn(new AuthenticationProperties { IsPersistent = false }, ident);
+                        HttpContext.GetOwinContext().Authentication.SignIn(new AuthenticationProperties { IsPersistent = false }, ident);
 
-                    return RedirectToAction("ComponentsGuide", "Home");
-                } else
+                        return RedirectToAction("ComponentsGuide", "Home");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("UserName", "Please make sure you entered the correct username.");
+                        ModelState.AddModelError("Password", "Please make sure you entered the correct password.");
+                        View();
+                    }
+                }
+                else
                 {
+                    ModelState.AddModelError("UserName", "Please make sure you entered the correct username.");
+                    ModelState.AddModelError("Password", "Please make sure you entered the correct password.");
                     return View();
                 }
             }
             return View();
         }
-        
+
         [HttpGet]
         public ActionResult Logoff()
         {
@@ -87,15 +101,49 @@ namespace LETS.Controllers
         [HttpPost]
         [ValidateAntiForgeryToken]
         [AllowAnonymous]
-        public ActionResult Register(RegisterUserViewModel registerUser)
+        public async Task<ActionResult> Register(RegisterUserViewModel registerUser)
         {
             if (registerUser != null && ModelState.IsValid)
             {
-                PasswordHashAndSalt passowordEncription = new PasswordHashAndSalt();
-                registerUser.Account.Password = passowordEncription.getHashedPassword(registerUser.Account.Password);
-                registerUser.Account.ConfirmPassword = passowordEncription.getHashedPassword(registerUser.Account.ConfirmPassword);
-                Context.RegisteredUsers.InsertOne(registerUser);
-                return RedirectToAction("RegisteredUsers");
+                var userByUsername = await Context.RegisteredUsers.Find(new BsonDocument {
+                    { "Account.UserName", registerUser.Account.UserName }
+                }).ToListAsync();
+
+                var userByEmail = await Context.RegisteredUsers.Find(new BsonDocument {
+                    { "Account.Email", registerUser.Account.Email }
+                }).ToListAsync();
+
+                if (userByUsername.Count == 0)
+                {
+                    if (userByEmail.Count == 0)
+                    {
+                        PasswordHashAndSalt passowordEncription = new PasswordHashAndSalt();
+                        registerUser.Account.Password = passowordEncription.getHashedPassword(registerUser.Account.Password);
+                        registerUser.Account.ConfirmPassword = passowordEncription.getHashedPassword(registerUser.Account.ConfirmPassword);
+                        Context.RegisteredUsers.InsertOne(registerUser);
+                        return RedirectToAction("RegisteredUsers");
+                    }
+                    else
+                    {
+                        registerUser.Account.Password = null;
+                        registerUser.Account.ConfirmPassword = null;
+                        ModelState.AddModelError("Account.Email", "Sorry, The following email already exists in our system.");
+                        return View(registerUser);
+                    }
+                }
+                else
+                {
+                    registerUser.Account.Password = null;
+                    registerUser.Account.ConfirmPassword = null;
+                    ModelState.AddModelError("Account.UserName", "Sorry, This username is not available.");
+
+                    if (userByEmail.Count > 0)
+                    {
+                        ModelState.AddModelError("Account.Email", "Sorry, The following email already exists in our system.");
+                    }
+
+                    return View(registerUser);
+                }
             }
             return View();
         }
