0dba46cd57f7a377cebb1e01f555cc2c8cf63da1
Kiran <kiran.rambha.2013@live.rhul.ac.uk>
2016-12-24 19:18:23 +0000

    UserProfile Tab Navigation Fix

--- LETS/LETS/Scripts/lets/userProfile.js
+++ LETS/LETS/Scripts/lets/userProfile.js
@@ -1,20 +1,4 @@
-﻿var changeProfile = function (element) {
-    if ($(element).hasClass("account_overview_toggle") && !$(element).hasClass("active")) {
-        $(element).addClass("active");
-        $(".account_settings_toggle").removeClass("active");
-        $("#account_overview_view").addClass("show").removeClass("hide");
-        $("#account_settings_view").removeClass("show").addClass("hide");
-    }
-
-    if ($(element).hasClass("account_settings_toggle") && !$(element).hasClass("active")) {
-        $(element).addClass("active");
-        $(".account_overview_toggle").removeClass("active");
-        $("#account_overview_view").addClass("hide").removeClass("show");
-        $("#account_settings_view").addClass("show").removeClass("hide");
-    }
-};
-
-function AccountSettings() {
+﻿function AccountSettings() {
     $("#account_settings_edit_button").slideUp();
     $(".user_details_table").slideUp();
     $.ajax({
